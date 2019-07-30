d528fe5c34e51c3ef953daeaa74e2bf468eb79f3
sinyyl <sinyyl11@gmail.com>
2019-07-30 13:33:17 -0700

    Lane keeping only

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,19 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
+#include "math.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+using std::atan2;
+using std::sin;
+using std::cos;
+using std::pow;
+using std::sqrt;
+
 
 int main() {
   uWS::Hub h;
@@ -49,9 +57,13 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+  //////////////////////////// my variables
+  int lane = 1;  // lane number
+  double ref_vel = 0.0;  // reference velocity in mph, start from 0
+  
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s,
+               &map_waypoints_dx, &map_waypoints_dy, &ref_vel, &lane]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -90,15 +102,164 @@ int main() {
 
           json msgJson;
 
+
+          //define the actual (x, y) points on map
           vector<double> next_x_vals;
           vector<double> next_y_vals;
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+          int prev_size = previous_path_x.size();
+          // Sensor Fusion part
+          if(prev_size > 0){
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+          // find ref_v to use
+          for(int i=0; i < sensor_fusion.size(); i++){
+            // cat is in my lane
+            double d = sensor_fusion[i][6];
+            if(d < (2 + 4 * lane + 2) && d > (2 + 4 * lane - 2)){
+
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(pow(vx, 2) + pow(vy, 2));
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += ((double)prev_size * 0.02 * check_speed);
+              // check_s values greater than mine and s gap (30 m)
+              // if the vehicle is in front and distance less than 30m
+              if((check_car_s > car_s) && ((check_car_s - car_s) < 30)){
+                
+                //ref_vel = 29.5; // mph
+                too_close = true;
+                if(lane > 0){
+                  lane = 0;
+                }
+              }
+
+            }
+          }
+          
+          if(too_close){
+            ref_vel -= 0.224;  // acceleration will be 5 m/s^2 
+          }else if(ref_vel < 49.5){
+            ref_vel += 0.224;
+          }
+          
+          
+          // Lane Following part
+          // a lsit of spaced x,y points for spline
+          vector<double> ptsx;
+          vector<double> ptsy;
+          // reference state of x, y, raw
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+           
+          if(prev_size < 2){
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }else{
+            // two of the previous points
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
+
+            double ref_x_prev = previous_path_x[prev_size - 2];
+            double ref_y_prev = previous_path_y[prev_size - 2];
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+            // store them into the vector
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          // create 3 points with 30m between two on Frenet corrdinate
+          int dist = 30;  // the distance for spline anker points
+          vector<double> next_wp0 = getXY(car_s + dist, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 2*dist, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 3*dist, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
 
 
+          for(int i =0; i < ptsx.size(); i++){
+
+            //shift car reference angle to 0 degrees
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = (shift_x * cos(0.0-ref_yaw) - shift_y * sin(0.0-ref_yaw));
+            ptsy[i] = (shift_x * sin(0.0-ref_yaw) + shift_y * cos(0.0-ref_yaw));
+          }
+
+
+          // a spline object
+          tk::spline s;
+
+          // set (x, y) points to spline
+          s.set_points(ptsx, ptsy);
+
+          // push the leftovers to the next path
+          for(int i=0; i < previous_path_x.size(); i++){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          // make the vehicle to go at the desired speed
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(pow(target_x, 2) + pow(target_y, 2));
+
+          double x_add_on = 0;
+          
+
+          // fill up the rest of out path planner with previous points, so the output is always 50 points
+          for(int i = 1; i <= 50 - previous_path_x.size(); i++){
+            double N = (target_dist / (0.02 * ref_vel / 2.24));
+            double x_point = x_add_on + (target_x) / N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+
+            // transfer the points back to the map coordinates
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+            y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+
+          }
+
+          // END
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////          
+          
+          
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
